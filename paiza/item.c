1575433b30cb14d35bc038423c7460366edb90eb
shunya-s15 <shunya.suraimu@gmail.com>
2020-07-15 13:07:55 +0900

    解けました

--- paiza/item.c
+++ paiza/item.c
@@ -1,29 +1,54 @@
-#include <stdio.h> //mitassei
-int N[400];
+#include <stdio.h> //完成しました。
+int N[200];
 int A[100];
 int B[100];
-int pattern = 0;
+int bit;
+int ans;
+int count=0;
 
-int set(int x,int y,int n){
-    int i,j;
-    A[x] = N[y];
-    if(n==1){
-        for(j=0;j<n;j++){
-            printf("%d ",A[j]);
-        }
-    }
-    for(i=1;i < 2*n-1;i++){
-        set(x+1,y+i,n-1);
+int zettaiti(int x){
+    if(x>0){
+        return x;
+    }else{
+        return -x;
     }
 }
+
 int main(void){
-    int i;
-    for(i=1;i<400;i++){
-        N[i] = i;
+    int i,j,l;
+    int x;
+    for(i=0;i<200;i++){
+        N[i] = i+1;
     }
     int n,k;
     scanf("%d%d",&n,&k);
-    for(i=1;i<2*n-1;i++){
-        set(0,i,n);
+    for(bit=0;bit<(1<<2*n);bit++){
+        j = 0;
+        l = 0;
+        ans = 0;
+        /*for(i=0;i<100;i++){
+            A[i]=0;
+            B[i]=0;
+        }*/
+        for(i=0;i<2*n;i++){
+            if(bit & (1<<i)){
+                A[j] = N[i];
+                j++;
+            }else{
+                B[l] = N[i];
+                l++;
+            }
+        }
+        if(j == l){
+            for(i=0;i<j;i++){
+                x = A[i]-B[i];
+                ans = ans + zettaiti(x);
+            }
+            if(ans < k+1){
+                count++;
+            }
+        }
     }
+    printf("%d\n",count);
+    return 0;
 }
